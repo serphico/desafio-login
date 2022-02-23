7ed7c31c44f25d2bad7315498548c24c33d74dda
emmanuel gomez <emmanuel.gomez@iMac-de-it.local>
2022-02-23 20:33:23 -0300

    desafio de session terminado

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const { Server: HttpServer} = require('http');
 const { Server: IOServer, Socket} = require('socket.io');
 const dotenv = require('dotenv');
-const {appRoute, chatRoute} = require('./routes/routes.js');
+const {appRoute, chatRoute, loginRoute} = require('./routes/routes.js');
 const chatCt = require('./controllers/chatController.js')
+const cookieParse = require('cookie-parser')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const advanceOptions = {useNewUrlParser:true, useUnifiedTopology:true}
 dotenv.config();
 
 const app = express();
@@ -17,13 +21,34 @@ app.use(express.urlencoded({extended:true}));
 
 app.use(express.json());
 
+app.use(cookieParse());
+
+app.use(session({
+
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URL,
+        mongoOptions: advanceOptions
+    }),
+
+    secret:'secretPhrase',
+    resave: false,
+    saveUninitialized: false ,
+
+    cookie:{
+        expires: 60000
+    }
+    
+}));
+
 app.use(express.static('./views'));
 
 app.set('view engine', 'pug');
 
 app.set('views', "./views");
 
-app.use('/', chatRoute);
+app.use('/', loginRoute);
+
+app.use('/chat', chatRoute);
 
 app.use('/api/productos-test', appRoute);
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,10 @@
 const {Router} = require('express');
 const prodGen = require('../controllers/prodGen.js')
-const chatCt = require('../controllers/chatController.js')
 
 
 const appRoute = Router();
 const chatRoute = Router();
+const loginRoute = Router();
 
 chatRoute.get('/', (req,res) => {
 
@@ -12,8 +12,43 @@ chatRoute.get('/', (req,res) => {
 })
 
 appRoute.get('/', (req, res) => {
-    let products = prodGen();
-    res.render('./layouts/productos.pug',{products})
+    console.log(req.session.username)
+    if(!req.session.username){
+        //res.render('')
+        setTimeout(() => {
+            res.redirect('/')
+        }, 500);
+    }else{
+        let products = prodGen();
+        let user = req.session.username
+        res.render('./layouts/productos.pug',{products,user})
+    }
+
 })
 
-module.exports = {appRoute,chatRoute};
+appRoute.post('/', (req, res) => {
+    let user = req.session.username
+
+    req.session.destroy( err =>{
+        if(err){
+            console.log(err)
+        }else{
+                res.render('./layouts/byeUser.pug',{user})
+        }
+    })
+
+})
+
+
+loginRoute.get('/', (req,res) => {
+
+    res.render('./layouts/login.pug')
+})
+
+loginRoute.post('/', (req,res) => {
+    console.log(req.body.name)
+    req.session.username = req.body.name
+    res.redirect('/api/productos-test')
+})
+
+module.exports = {appRoute,chatRoute,loginRoute};
